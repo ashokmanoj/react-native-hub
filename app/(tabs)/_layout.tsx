ae1c9bcab38fb55f240b9deea12cc72e5626e333
Manu <manojc6363530865@gmail.com>
2025-10-10 08:56:02 +0530

    Learnt about view, text, stack, tabs, stand and tab screen, style, tabBaricons and tintcolor in react native

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
+import { Tabs } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon(props) {
+            return <FontAwesome name="home" size={24} color="black" />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="login"
+        options={{
+          title: "Login",
+          tabBarIcon(props) {
+            return <SimpleLineIcons name="login" size={24} color="black" />;
+          },
+        }}
+      />
+    </Tabs>
+  );
+}
